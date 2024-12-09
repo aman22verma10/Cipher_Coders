19249ceea70eec3d0b83e3fa696a5fe520ad83d6
aman verma <aman22verma10@gmail.com>
2024-12-09 17:18:08 +0530

    first commit

---
+++ Beginner_level_Question01/Ques3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number, square;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    square = number * number;
+
+    cout << "The square of " << number << " is: " << square << endl;
+
+    return 0;
+}
