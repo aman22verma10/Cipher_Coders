6544dece502eba5ded3495363c593636c81d7674
aman verma <aman22verma10@gmail.com>
2024-12-09 17:25:33 +0530

    second

--- Beginner_level_Question02/Ques1.cpp
+++ Beginner_level_Question02/Ques1.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() {
-    string str1, str2;
-    
-    cout << "Enter the first string: ";
-    cin >> str1;
-    
-    cout << "Enter the second string: ";
-    cin >> str2;
-    
-    string concatenated = str1 + str2;
-    
-    cout << "Concatenated string: " << concatenated << endl;
-    
+    int num1, num2, num3, largest;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+    cout << "Enter the third number: ";
+    cin >> num3;
+
+    if (num1 >= num2 && num1 >= num3) {
+        largest = num1;
+    } else if (num2 >= num1 && num2 >= num3) {
+        largest = num2;
+    } else {
+        largest = num3;
+    }
+
+    cout << "The largest number is: " << largest << endl;
+
     return 0;
 }
